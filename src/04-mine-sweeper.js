f9e006652d268ccd20a79f6e3ca83d9c344ef6ca
ocnelias <ocnelias@gmail.com>
2021-04-08 12:14:21 +0300

    feat: solve task4

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,67 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  const newArr = [];
+  for (let i = 0; i < matrix.length; i++) {
+    newArr.push([]);
+    for (let k = 0; k < matrix[i].length; k++) {
+      newArr[i].push(0);
+    }
+  }
+  for (let i = 0; i < matrix.length; i++) {
+    for (let k = 0; k < matrix[i].length; k++) {
+      // top direction
+      if (matrix[i - 1] !== undefined) {
+        if (matrix[i - 1][k] === true) {
+          newArr[i][k] += 1;
+        }
+      }
+      // top right direction
+      if (matrix[i - 1] !== undefined && matrix[k + 1] !== undefined) {
+        if (matrix[i - 1][k + 1] === true) {
+          newArr[i][k] += 1;
+        }
+      }
+      // right direction
+      if (matrix[k + 1] !== undefined) {
+        if (matrix[i][k + 1] === true) {
+          newArr[i][k] += 1;
+        }
+      }
+      // bottom right direction
+      if (matrix[i + 1] !== undefined && matrix[k + 1] !== undefined) {
+        if (matrix[i + 1][k + 1] === true) {
+          newArr[i][k] += 1;
+        }
+      }
+      // bottom direction
+      if (matrix[i + 1] !== undefined) {
+        if (matrix[i + 1][k] === true) {
+          newArr[i][k] += 1;
+        }
+      }
+      // bottom left direction
+      if (matrix[i + 1] !== undefined && matrix[k - 1] !== undefined) {
+        if (matrix[i + 1][k - 1] === true) {
+          newArr[i][k] += 1;
+        }
+      }
+      // left direction
+      if (matrix[k - 1] !== undefined) {
+        if (matrix[i][k - 1] === true) {
+          newArr[i][k] += 1;
+        }
+      }
+      // top left direction
+      if (matrix[i - 1] !== undefined && matrix[k - 1] !== undefined) {
+        if (matrix[i - 1][k - 1] === true) {
+          newArr[i][k] += 1;
+        }
+      }
+    }
+  }
+  return newArr;
 }
 
 module.exports = minesweeper;
